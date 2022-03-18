346425642e49fbd6c185ccb1f39c02d962587910
nathan-pj <nathan.pagejones@gmail.com>
2022-03-18 13:50:03 +0000

    revert route changes to normal

--- nc-news/src/App.js
+++ nc-news/src/App.js
@@ -1,97 +1,87 @@
 import "./css/App.css";
 import { useState } from "react";
-import { HashRouter, Route, Routes } from "react-router-dom";
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import NavBar from "./Components/nav/Nav";
+
 import DisplayAllArticles from "./Components/pages/mainPage/DisplayAllArticles";
 import DisplaySingleArticle from "./Components/pages/singleArticlePage/DisplaySingleArticle";
 import DisplayArticles from "./Components/pages/mainPage/DisplayArticles";
+
 import WriteArticle from "./Components/pages/writeArticlePage/WriteArticle";
 
 import NotFound from "./Components/NotFound";
 import Info from "./Components/pages/infoPage/Info";
-import { BrowserRouter } from "react-router-dom";
 
 function App() {
   const [signedIn, setSignedIn] = useState("jessjelly");
   const [sortBy, setSortBy] = useState("");
   const [isLoading, setIsLoading] = useState(true);
   const [deletedArticle, setDeletedArticle] = useState("");
-  console.log(process.env.PUBLIC_URL);
   return (
     <BrowserRouter>
-      <>
-        <NavBar
-          setSortBy={setSortBy}
-          isLoading={isLoading}
-          setIsLoading={setIsLoading}
-        />
-        <div className="App">
-          <Routes basename="/fe-news-app">
-            <Route
-              exact
-              path={process.env.PUBLIC_URL + "/"}
-              element={
-                <DisplayAllArticles
-                  sortBy={sortBy}
-                  setSortBy={setSortBy}
-                  isLoading={isLoading}
-                  setIsLoading={setIsLoading}
-                  signedIn={signedIn}
-                />
-              }
-            />
-            <Route
-              exact
-              path={process.env.PUBLIC_URL + "/articles"}
-              element={
-                <DisplayAllArticles
-                  sortBy={sortBy}
-                  setSortBy={setSortBy}
-                  isLoading={isLoading}
-                  setIsLoading={setIsLoading}
-                  signedIn={signedIn}
-                />
-              }
-            />
-            <Route
-              path={process.env.PUBLIC_URL + "/articles/:id"}
-              element={
-                <DisplaySingleArticle
-                  isLoading={isLoading}
-                  setIsLoading={setIsLoading}
-                  deletedArticle={deletedArticle}
-                  setDeletedArticle={setDeletedArticle}
-                  signedIn={signedIn}
-                />
-              }
-            />
-            <Route
-              path={process.env.PUBLIC_URL + "/articles/:topic"}
-              element={
-                <DisplayArticles
-                  sortBy={sortBy}
-                  setSortBy={setSortBy}
-                  isLoading={isLoading}
-                  setIsLoading={setIsLoading}
-                  deletedArticle={deletedArticle}
-                  setDeletedArticle={setDeletedArticle}
-                  signedIn={signedIn}
-                />
-              }
-            />
-            <Route
-              exact
-              path={process.env.PUBLIC_URL + "/write-article"}
-              element={<WriteArticle />}
-            />
-            <Route path={process.env.PUBLIC_URL + "/info"} element={<Info />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </div>
-      </>
+      <NavBar
+        setSortBy={setSortBy}
+        isLoading={isLoading}
+        setIsLoading={setIsLoading}
+      />
+      <div className="App">
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <DisplayAllArticles
+                sortBy={sortBy}
+                setSortBy={setSortBy}
+                isLoading={isLoading}
+                setIsLoading={setIsLoading}
+                signedIn={signedIn}
+              />
+            }
+          />
+          <Route
+            path="/articles"
+            element={
+              <DisplayAllArticles
+                sortBy={sortBy}
+                setSortBy={setSortBy}
+                isLoading={isLoading}
+                setIsLoading={setIsLoading}
+                signedIn={signedIn}
+              />
+            }
+          />
+          <Route
+            path="/articles/:id"
+            element={
+              <DisplaySingleArticle
+                isLoading={isLoading}
+                setIsLoading={setIsLoading}
+                deletedArticle={deletedArticle}
+                setDeletedArticle={setDeletedArticle}
+                signedIn={signedIn}
+              />
+            }
+          />
+          <Route
+            path="/articles/:topic"
+            element={
+              <DisplayArticles
+                sortBy={sortBy}
+                setSortBy={setSortBy}
+                isLoading={isLoading}
+                setIsLoading={setIsLoading}
+                deletedArticle={deletedArticle}
+                setDeletedArticle={setDeletedArticle}
+                signedIn={signedIn}
+              />
+            }
+          />
+          <Route path="/write-article" element={<WriteArticle />} />
+          <Route path="/info" element={<Info />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </div>
     </BrowserRouter>
   );
 }
-
 export default App;
